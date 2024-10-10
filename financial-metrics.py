1b67fe5657c809778adf62641771fba5267bbebd
spacedust10 <vinit_gunaki@protonmail.com>
2024-10-10 17:55:48 +0530

    feat/analyzer: calculate savings

--- financial-metrics.py
+++ financial-metrics.py
@@ -9,7 +9,13 @@ class FinancialAnalyzer:
         yearly_income = self.data.loc[self.data['Expense/Income'] == 'Income'].groupby('Year')['Amount()'].sum().mean()
         yearly_expenses = self.data.loc[self.data['Expense/Income'] == 'Expense'].groupby('Year')['Amount()'].sum().mean()
         
+        savings_yearly = yearly_income - yearly_expenses
+        savings_rate_yearly = (savings_yearly / yearly_income) * 100 if yearly_income > 0 else 0
+        
         top_expenses = self.data.loc[self.data['Expense/Income'] == 'Expense'].groupby('Category')['Amount()'].sum().sort_values(ascending=False)
         
         monthly_income = self.data.loc[self.data['Expense/Income'] == 'Income'].groupby(self.data['Date'].dt.to_period('M'))['Amount()'].sum().mean()
         monthly_expenses = self.data.loc[self.data['Expense/Income'] == 'Expense'].groupby(self.data['Date'].dt.to_period('M'))['Amount()'].sum().mean()
+        
+        savings_monthly = yearly_income - yearly_expenses
+        savings_rate_monthly = (savings_monthly / yearly_income) * 100 if monthly_income > 0 else 0
