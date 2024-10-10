b38e5c8258295b017d33260dfc7f86f8ec1ad34f
spacedust10 <vinit_gunaki@protonmail.com>
2024-10-10 19:04:30 +0530

    feat/visualization: Function for making Bar charts

--- data-visualization.py
+++ data-visualization.py
@@ -7,8 +7,8 @@ def make_pie_chart(df, year, label):
     pie_fig = px.pie(sub_df, values='Amount (EUR)', names='Category', color_discrete_sequence=color_scale)
     pie_fig.update_traces(textposition='inside', direction='clockwise', hole=0.3, textinfo="label+percent")
     
-    total_expense = df[(df['Expense/Income'] == 'Expense') & (df['Year'] == year)]['Amount (EUR)'].sum() 
-    total_income = df[(df['Expense/Income'] == 'Income') & (df['Year'] == year)]['Amount (EUR)'].sum()
+    total_expense = df[(df['Expense/Income'] == 'Expense') & (df['Year'] == year)]['Amount'].sum() 
+    total_income = df[(df['Expense/Income'] == 'Income') & (df['Year'] == year)]['Amount'].sum()
     
     if label == 'Expense':
         total_text = "€ " + str(round(total_expense))
@@ -25,3 +25,19 @@ def make_pie_chart(df, year, label):
         annotations=[dict(text=total_text, x=0.5, y=0.5, font_size=12, showarrow=False)]
     )
     return pie_fig
+
+def make_monthly_bar_chart(df, year, label):
+    df = df[(df['Expense/Income'] == label) & (df['Year'] == year)]
+    total_by_month = (df.groupby(['Month', 'Month Name'])['Amount']
+                        .sum()
+                        .to_frame()
+                        .reset_index()
+                        .sort_values(by='Month')
+                        .reset_index(drop=True))
+    if label == "Income":
+        color_scale = px.colors.sequential.YlGn
+    else:
+        color_scale = px.colors.sequential.OrRd
+    
+    bar_fig = px.bar(total_by_month, x='Month Name', y='Amount', text_auto='.2s', title=label + " per month", color='Amount', color_continuous_scale=color_scale)
+    return bar_fig
