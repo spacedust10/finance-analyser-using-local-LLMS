cd8059cd4ef45b58410ea9f74cb0b9e1f3f5af63
spacedust10 <vinit_gunaki@protonmail.com>
2024-10-10 17:47:49 +0530

    feat/analyzer: calculate yearly income/expense

--- financial-metrics.py
+++ financial-metrics.py
@@ -3,3 +3,8 @@ import pandas as pd
 class FinancialAnalyzer:
     def __init__(self, data: pd.DataFrame):
         self.data = data
+    
+    def financial_analysis(self):
+        key_figures = {}
+        yearly_income = self.data.loc[self.data['Expense/Income'] == 'Income'].groupby('Year')['Amount()'].sum().mean()
+        yearly_expenses = self.data.loc[self.data['Expense/Income'] == 'Expense'].groupby('Year')['Amount()'].sum().mean()
